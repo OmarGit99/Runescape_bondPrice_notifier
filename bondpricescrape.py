6ec5e57e031d09a1f639d612389c7a53b495f801
omarshaikh <omarshaikh@outlook.com>
2021-06-23 15:18:06 +0530

    added price limit at 28m gp

--- bondpricescrape.py
+++ bondpricescrape.py
@@ -8,20 +8,24 @@ soup = bs4.BeautifulSoup(page.text, 'html.parser')
 bond_price = soup.select('.infobox-quantity-replace')
 price = bond_price[0].text
 
-port = 465  # For SSL
-password = "bonds2themoon"
-sender_email = "bondbot4rs@gmail.com"
-receiver_email = "omarshaikh@outlook.com"
-message = """\
-Subject: BOND ALERT BY BONDBOT4RS
+price_int = int(price.replace(",", ""))
 
-Hi! \n the price of bonds have reached your limit and are currently at - """+ price + " gold pieces."
+if(price_int >= 28000000):
+    port = 465  # For SSL
+    password = "bonds2themoon"
+    sender_email = "bondbot4rs@gmail.com"
+    receiver_email = "omarshaikh@outlook.com"
+    message = """\
+    Subject: BOND ALERT BY BONDBOT4RS
 
-# Create a secure SSL context
-context = ssl.create_default_context()
+    Hi! \n the price of bonds have reached your limit and are currently at - """+ price + " gold pieces."
 
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    server.login("bondbot4rs@gmail.com", password)
-    # TODO: Send email here
-    server.sendmail(sender_email, receiver_email, message)
+    # Create a secure SSL context
+    context = ssl.create_default_context()
 
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login("bondbot4rs@gmail.com", password)
+        # TODO: Send email here
+        server.sendmail(sender_email, receiver_email, message)
+else:
+    print("Price lower then limit! :\(")
